9694b0ca676ce73486e371e64e4d848322ba45e7
migarbo1 <miguel.98.sueca@hotmail.com>
2020-03-31 19:12:29 +0200

    9 puzzle done

---
+++ Assets/Scripts/Puzzles/9-puzzle/Partial_image.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Partial_image : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] Sprite subImage;
+    private Vector3 ini,fin;
+    private Partial_image source;
+
+    public object x { get; private set; }
+
+    public void setSubImage(Sprite s) {
+        GetComponent<SpriteRenderer>().sprite = s;
+    }
+
+    public void setPosition(float x, float y) {
+        transform.position = new Vector3(x, y, transform.position.z);
+    }
+
+
+
+    public Vector3 GetSector(float x, float y) {
+
+        Vector3 point;
+
+        if (x < -0.045)
+        {
+            //columna de la izquierda
+            if (y < -1.54)
+            {
+                //fila de inferior
+                point = new Vector3(-1.09f, -3.04f, transform.position.z);
+            }
+            else {
+                if (y < 1.43)
+                {
+                    //fila central
+                    point = new Vector3(-1.09f, -0.07f, transform.position.z);
+                }
+                else {
+                    //fila superior
+                    point = new Vector3(-1.09f, 2.9f, transform.position.z);
+                }
+            }
+        }
+        else {
+            if (x < 2.045)
+            {
+                //columna central
+                if (y < -1.54)
+                {
+                    //fila de inferior
+                    point = new Vector3(1, -3.04f, transform.position.z);
+                }
+                else
+                {
+                    if (y < 1.43)
+                    {
+                        //fila central
+                        point = new Vector3(1, -0.07f, transform.position.z);
+                    }
+                    else
+                    {
+                        //fila superior
+                        point = new Vector3(1, 2.9f, transform.position.z);
+                    }
+                }
+            }
+            else {
+                //columna de la derecha
+                if (y < -1.54)
+                {
+                    //fila de inferior
+                    point = new Vector3(3.09f, -3.04f, transform.position.z);
+                }
+                else
+                {
+                    if (y < 1.43)
+                    {
+                        //fila central
+                        point = new Vector3(3.09f, -0.07f, transform.position.z);
+                    }
+                    else
+                    {
+                        //fila superior
+                        point = new Vector3(3.09f, 2.9f, transform.position.z);
+                    }
+                }
+            }
+        }
+        return point;
+    }
+
+    private void OnMouseDown() {
+        source = this;
+        Vector2 mouseMove = GetMousePosition();
+        ini = GetSector(mouseMove.x,mouseMove.y);
+    }
+    private void OnMouseDrag()
+    {
+        Vector2 mousePos = GetMousePosition();
+        this.gameObject.transform.localPosition = new Vector3(mousePos.x,mousePos.y,this.gameObject.transform.localPosition.z );
+
+    }
+
+    private void OnMouseUp() {
+        Vector2 mouseMove = GetMousePosition();
+
+        fin = GetSector(mouseMove.x, mouseMove.y);
+        this.gameObject.transform.localPosition = new Vector3(fin.x, fin.y, fin.z);
+
+        Partial_image[] allObjects = UnityEngine.Object.FindObjectsOfType<Partial_image>();
+        foreach (Partial_image go in allObjects)
+            if (go.transform.localPosition == fin && go.Equals(this) == false) {
+                go.setPosition(ini.x, ini.y);
+                break;
+            } 
+    }
+
+    private bool isBetween(float a, float b, float c)
+    {
+        return a >= b && a <= c;
+    }
+    private static Vector2 GetMousePosition() => Camera.main.ScreenToWorldPoint(Input.mousePosition);
+}
---
+++ Assets/Scripts/Puzzles/9-puzzle/Set_images.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Set_images : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private Partial_image partial_im;
+    [SerializeField] private Sprite[] images;
+    private static Random rng = new Random();
+    private float[] posX = new float[9] { -1.09f, 1, 3.09f, -1.09f, 1, 3.09f, -1.09f, 1, 3.09f };
+    private float[] posY = new float[9] { 2.9f, 2.9f, 2.9f, -0.07f,  -0.07f, -0.07f, -3.04f, -3.04f, -3.04f };
+    public Dictionary<string, Vector3> correct = new Dictionary<string, Vector3>();
+
+    [SerializeField] private GameObject EndgameMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EndgameMenu.SetActive(false);
+        for (int i = 0; i < images.Length; i++)
+        {
+            Vector3 aux = new Vector3(posX[i], posY[i], 0);
+            correct.Add(images[i].name,aux);
+        }
+        /*
+        foreach (string s in correct.Keys) {
+            Debug.Log(correct[s]);
+        }
+
+        Debug.Log("----------------");
+        */
+        reshuffle(images);
+        
+        for (int i = 0; i < images.Length; i++)
+        {
+            Partial_image subImage = Instantiate(partial_im) as Partial_image;
+            subImage.setSubImage(images[i]);
+            subImage.setPosition(posX[i], posY[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        check_solution();
+    }
+
+
+    void reshuffle(Sprite[] sprites)
+    {
+        // Knuth shuffle algorithm :: courtesy of Wikipedia :)
+        for (int t = 0; t < sprites.Length; t++)
+        {
+            Sprite tmp = sprites[t];
+            int r = Random.Range(t, sprites.Length);
+            sprites[t] = sprites[r];
+            sprites[r] = tmp;
+        }
+    }
+
+    private void check_solution() {
+        bool completed = true;
+        Partial_image[] allObjects = UnityEngine.Object.FindObjectsOfType<Partial_image>();
+        foreach (Partial_image go in allObjects)
+        {
+            string name = go.GetComponent<SpriteRenderer>().sprite.name;
+
+            Vector3 pos = go.transform.localPosition;
+            if (correct[name] != pos)
+            {
+                completed = false;
+            }
+        }
+        if (completed) { StartCoroutine(puzzle_completed()); }
+    }
+
+    private IEnumerator puzzle_completed() {
+        yield return new WaitForSeconds(0.75f);
+
+        EndgameMenu.SetActive(true);
+
+    }
+}
