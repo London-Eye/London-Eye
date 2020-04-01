c339361349a99d0878fb65b1661ab1d198adba4d
migarbo1 <miguel.98.sueca@hotmail.com>
2020-04-02 00:50:09 +0200

    probetas completado a falta de GUI de endgame

--- Assets/Scripts/Puzzles/probetas/GameController.cs
+++ Assets/Scripts/Puzzles/probetas/GameController.cs
@@ -4,18 +4,33 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    [SerializeField] public PauseController ps;
     public bool first;
-    public circle ballTomove;
+    public Color ballTomove;
     public initially_full_tube sourceTube;
+    public bool endgame;
+    private initially_full_tube[] tubes;
     // Start is called before the first frame update
     void Start()
     {
         first = true;
+        endgame = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        endgame = isEndgame();
+    }
+
+    private bool isEndgame() {
+        tubes = GameObject.FindObjectsOfType<initially_full_tube>();
+        for (int i = 0; i < tubes.Length; i++) {
+            if (!tubes[i].isComplete()) {
+                return false;
+            }
+        }
+        return true;
+
     }
 }
--- Assets/Scripts/Puzzles/probetas/initially_full_tube.cs
+++ Assets/Scripts/Puzzles/probetas/initially_full_tube.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 
 public class initially_full_tube : MonoBehaviour
 {
-    [SerializeField] circle[] Balls;
     [SerializeField] string initial_status;
     private GameController controller;
-    public Stack<circle> pila;
+    public Stack<Color> pila;
     private int max_size = 4;
     private GameObject basse;
     private GameObject middleBase;
@@ -17,7 +16,7 @@ public class initially_full_tube : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        pila = new Stack<circle>();
+        pila = new Stack<Color>();
         controller = GameObject.Find("GameController").GetComponent<GameController>();
         basse = this.gameObject.transform.GetChild(0).gameObject;
         middleBase = this.gameObject.transform.GetChild(1).gameObject;
@@ -26,15 +25,10 @@ public class initially_full_tube : MonoBehaviour
 
         if (initial_status == "full")
         {
-            for (int i = Balls.Length - 1; i >= 0; i--)
-            {
-                pila.Push(Balls[i]);
-            }
-            basse.GetComponent<SpriteRenderer>().color = Balls[0].getColor();
-            middleBase.GetComponent<SpriteRenderer>().color = Balls[1].getColor();
-            middleTop.GetComponent<SpriteRenderer>().color = Balls[2].getColor();
-            top.GetComponent<SpriteRenderer>().color = Balls[3].getColor();
-            oldY = top.transform.position.y;
+            pila.Push(basse.GetComponent<SpriteRenderer>().color);
+            pila.Push(middleBase.GetComponent<SpriteRenderer>().color);
+            pila.Push(middleTop.GetComponent<SpriteRenderer>().color);
+            pila.Push(top.GetComponent<SpriteRenderer>().color);
         }
         else {
             basse.SetActive(false);
@@ -47,90 +41,192 @@ public class initially_full_tube : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     private void OnMouseDown() {
-        Debug.Log(controller.first);
-        if (size(pila) > 0 && controller.first) //primera bola que pillas
-        {
-            controller.ballTomove = pila.Peek();
-            controller.first = !controller.first;
-            Debug.Log(controller.ballTomove.ToString());
-            controller.sourceTube = this;
-            oldY = top.transform.position.y;
-            top.transform.position = new Vector3(top.transform.position.x, oldY + 0.65f, top.transform.position.z);
-        }
-        else
-        {
-            if (size(pila) < max_size) //con la bola ya pillada, si la bola cabe
+
+        if (!controller.ps.IsPaused && !controller.endgame) {
+
+            if (controller.first) //primera bola que pillas
             {
-                if (controller.ballTomove != null) //  y la bola no es null
+                if (size(this.pila) > 0) //si has clicado una probeta con bolas
                 {
-                    controller.sourceTube.pila.Pop();
-                    pila.Push(controller.ballTomove);
-                    Debug.Log(pila.ToArray().ToString());
-                    top.transform.position = new Vector3(top.transform.position.x, oldY, top.transform.position.z);
-                    printStackStatus(pila);
+                    Debug.Log("First");
+                    controller.ballTomove = this.pila.ToArray()[0];
+                    controller.first = !controller.first;
+                    controller.sourceTube = this;
+                    doAnimation(this);
                 }
             }
-            else { // la bola no cabe
-                controller.sourceTube.gameObject.transform.GetChild(3).transform.position = new Vector3(controller.sourceTube.gameObject.transform.GetChild(3).transform.position.x, controller.sourceTube.oldY, controller.sourceTube.gameObject.transform.GetChild(3).transform.position.z);
-                printStackStatus(pila);
+            else
+            {
+                //TODO: 
+                // - que la probeta esté vacía o que tenga en la pila a alguien de su color.
+
+                if (size(this.pila) < max_size) //con la bola ya pillada, si la bola cabe
+                {
+                    Debug.Log("Cabe");
+                    if (size(this.pila) == 0) {
+                        Debug.Log("Case vacia");
+                        undoAnimation(controller.sourceTube);
+                        this.pila.Push(controller.sourceTube.pila.Pop());
+                    }
+                    else {
+                        Debug.Log("case con algo");
+                        if (this.pila.ToArray()[0].Equals(controller.ballTomove))
+                        {
+                            Debug.Log("Es algo del mismo color");
+                            undoAnimation(controller.sourceTube);
+                            this.pila.Push(controller.sourceTube.pila.Pop());
+                            
+                            printStackStatus(this.pila, this);
+                        }
+                        else {
+                            Debug.Log("no es del mismo color");
+                            undoAnimation(controller.sourceTube);
+                            printStackStatus(controller.sourceTube.pila, controller.sourceTube);
+                        }
+                    }/*
+                    undoAnimation(controller.sourceTube);
+                    printStackStatus(this.pila, this);*/
+
+                    printStackStatus(this.pila, this);
+                    printStackStatus(controller.sourceTube.pila, controller.sourceTube);
+                }
+                else
+                { // la bola no cabe
+                    Debug.Log("No cabe");
+                     undoAnimation(controller.sourceTube);
+                     printStackStatus(this.pila, this);
+                }
+                controller.ballTomove = Color.white;
+                controller.first = !controller.first;
+                controller.sourceTube = null;
             }
-            controller.ballTomove = null;
-            controller.first = !controller.first;
-            controller.sourceTube = null;
         }
-        
+
     }
 
-    public int size(Stack<circle> s) {
+    public int size(Stack<Color> s) {
         return s.ToArray().Length;
     }
 
-    public void printStackStatus(Stack<circle> s) {
+    public void printStackStatus(Stack<Color> s, initially_full_tube tb) {
 
         switch (size(s))
         {
             case 0:
-                basse.SetActive(false);
-                middleBase.SetActive(false);
-                middleTop.SetActive(false);
-                top.SetActive(false);
+                tb.basse.SetActive(false);
+                tb.middleBase.SetActive(false);
+                tb.middleTop.SetActive(false);
+                tb.top.SetActive(false);
                 break;
 
             case 1:
-                basse.SetActive(true);
-                basse.GetComponent<SpriteRenderer>().color = s.ToArray()[0].getColor();
+                tb.basse.SetActive(true);
+                tb.basse.GetComponent<SpriteRenderer>().color = s.ToArray()[0];
+                tb.middleBase.SetActive(false);
+                tb.middleTop.SetActive(false);
+                tb.top.SetActive(false);
+                //Debug.Log("case 1");
                 break;
 
             case 2:
-                basse.SetActive(true);
-                basse.GetComponent<SpriteRenderer>().color = s.ToArray()[0].getColor();
-                middleBase.SetActive(true);
-                middleBase.GetComponent<SpriteRenderer>().color = s.ToArray()[1].getColor();
+                tb.basse.SetActive(true);
+                tb.basse.GetComponent<SpriteRenderer>().color = s.ToArray()[1];
+                tb.middleBase.SetActive(true);
+                tb.middleBase.GetComponent<SpriteRenderer>().color = s.ToArray()[0];
+                tb.middleTop.SetActive(false);
+                tb.top.SetActive(false);
+               // Debug.Log("case 2");
                 break;
 
             case 3:
-                basse.SetActive(true);
-                basse.GetComponent<SpriteRenderer>().color = s.ToArray()[0].getColor();
-                middleBase.SetActive(true);
-                middleBase.GetComponent<SpriteRenderer>().color = s.ToArray()[1].getColor();
-                middleTop.SetActive(true);
-                middleTop.GetComponent<SpriteRenderer>().color = s.ToArray()[2].getColor();
+                tb.basse.SetActive(true);
+                tb.basse.GetComponent<SpriteRenderer>().color = s.ToArray()[2];
+                tb.middleBase.SetActive(true);
+                tb.middleBase.GetComponent<SpriteRenderer>().color = s.ToArray()[1];
+                tb.middleTop.SetActive(true);
+                tb.middleTop.GetComponent<SpriteRenderer>().color = s.ToArray()[0];
+                tb.top.SetActive(false);
+                //Debug.Log("case 3");
                 break;
 
             case 4:
-                basse.SetActive(true);
-                basse.GetComponent<SpriteRenderer>().color = s.ToArray()[0].getColor();
-                middleBase.SetActive(true);
-                middleBase.GetComponent<SpriteRenderer>().color = s.ToArray()[1].getColor();
-                middleTop.SetActive(true);
-                middleTop.GetComponent<SpriteRenderer>().color = s.ToArray()[2].getColor();
-                top.SetActive(true);
-                top.GetComponent<SpriteRenderer>().color = s.ToArray()[3].getColor();
+                tb.basse.SetActive(true);
+                tb.basse.GetComponent<SpriteRenderer>().color = s.ToArray()[3];
+                tb.middleBase.SetActive(true);
+                tb.middleBase.GetComponent<SpriteRenderer>().color = s.ToArray()[2];
+                tb.middleTop.SetActive(true);
+                tb.middleTop.GetComponent<SpriteRenderer>().color = s.ToArray()[1];
+                tb.top.SetActive(true);
+                tb.top.GetComponent<SpriteRenderer>().color = s.ToArray()[0];
+                //Debug.Log("case 4");
                 break;
-
+            default:
+                break;
         }
+    }
+    public bool isComplete() {
+        if (size(this.pila) == 0)
+        {
+            return true;
+        }
+        else {
+            if (size(this.pila) == 4)
+            {
+                for (int i = 0; i < size(this.pila) - 1; i++)
+                {
+                    if (!this.pila.ToArray()[i].Equals(this.pila.ToArray()[i + 1]))
+                    {
+                        return false;
+                    }
+                }
+            }
+            else {
+                return false;
+            }
+        }
+        return true;
+    }
+    public void doAnimation(initially_full_tube tb) {
+        switch (size(tb.pila)) {
+            case 1:
+                oldY = tb.basse.transform.position.y;
+                tb.basse.transform.position = new Vector3(tb.basse.transform.position.x, tb.oldY + 0.65f, tb.basse.transform.position.z);
+                break;
+            case 2:
+                oldY = tb.middleBase.transform.position.y;
+                tb.middleBase.transform.position = new Vector3(tb.middleBase.transform.position.x, tb.oldY + 0.65f, tb.middleBase.transform.position.z);
+                break;
+            case 3:
+                oldY = tb.middleTop.transform.position.y;
+                tb.middleTop.transform.position = new Vector3(tb.middleTop.transform.position.x, tb.oldY + 0.65f, tb.middleTop.transform.position.z);
+                break;
+            case 4:
+                oldY = tb.top.transform.position.y;
+                tb.top.transform.position = new Vector3(tb.top.transform.position.x, tb.oldY + 0.65f, tb.top.transform.position.z);
+                break;
+        }
+    }
+
+    public void undoAnimation(initially_full_tube tb) {
+        
+        switch (size(tb.pila))
+        {
+            case 1:
+                tb.basse.transform.position = new Vector3(tb.basse.transform.position.x, tb.oldY, tb.basse.transform.position.z);
+                break;
+            case 2:
+                tb.middleBase.transform.position = new Vector3(tb.middleBase.transform.position.x, tb.oldY, tb.middleBase.transform.position.z);
+                break;
+            case 3:
+                tb.middleTop.transform.position = new Vector3(tb.middleTop.transform.position.x, tb.oldY, tb.middleTop.transform.position.z);
+                break;
+            case 4:
+                tb.top.transform.position = new Vector3(tb.top.transform.position.x, tb.oldY, tb.top.transform.position.z);
+                break;
+        }
+        
     }
 }
