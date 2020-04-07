0b41a2b1df6cc0a9067004aedaccd905f8e4ea78
Alejandro Gómez <algono@inf.upv.es>
2020-04-07 18:05:50 +0200

    MoveTheBlock sprites as icons

--- Assets/Scripts/Puzzles/MoveTheBlock/BlockMovement.cs
+++ Assets/Scripts/Puzzles/MoveTheBlock/BlockMovement.cs
@@ -2,25 +2,25 @@
 
 public class BlockMovement : MonoBehaviour
 {
+    public Transform bounds;
+    public BoxCollider2D moveCollider;
+
     public float speed = 5f;
 
     private const float colliderSizeReductionFactor = 0.9f;
-
 
     private Vector2 directionAxis;
     private float offset;
 
     private Rigidbody2D _rb;
-    private BoxCollider2D _collider;
 
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-        _collider = GetComponent<BoxCollider2D>();
 
-        directionAxis = transform.localScale.y > transform.localScale.x ? Vector2.up : Vector2.right;
-        offset = transform.localScale.y > transform.localScale.x ? transform.position.y : transform.position.x;
+        directionAxis = bounds.transform.localScale.y > bounds.transform.localScale.x ? Vector2.up : Vector2.right;
+        offset = bounds.transform.localScale.y > bounds.transform.localScale.x ? transform.position.y : transform.position.x;
         offset -= Mathf.FloorToInt(offset);
     }
 
@@ -31,11 +31,11 @@ public class BlockMovement : MonoBehaviour
         // Reduce a bit the collider size opposite to the block's direction, to avoid unwanted collisions
         if (directionAxis.y > 0)
         {
-            _collider.size = new Vector2(_collider.size.x * colliderSizeReductionFactor, _collider.size.y);
+            moveCollider.size = new Vector2(moveCollider.size.x * colliderSizeReductionFactor, moveCollider.size.y);
         }
         else
         {
-            _collider.size = new Vector2(_collider.size.x, _collider.size.y * colliderSizeReductionFactor);
+            moveCollider.size = new Vector2(moveCollider.size.x, moveCollider.size.y * colliderSizeReductionFactor);
         }
     }
 
@@ -49,11 +49,11 @@ public class BlockMovement : MonoBehaviour
         // Increase collider size opposite to the block's direction back to its original size
         if (directionAxis.y > 0)
         {
-            _collider.size = new Vector2(_collider.size.x / colliderSizeReductionFactor, _collider.size.y);
+            moveCollider.size = new Vector2(moveCollider.size.x / colliderSizeReductionFactor, moveCollider.size.y);
         }
         else
         {
-            _collider.size = new Vector2(_collider.size.x, _collider.size.y / colliderSizeReductionFactor);
+            moveCollider.size = new Vector2(moveCollider.size.x, moveCollider.size.y / colliderSizeReductionFactor);
         }
     }
 
@@ -76,13 +76,13 @@ public class BlockMovement : MonoBehaviour
         if (directionAxis.y > 0)
         {
             float y = Mathf.RoundToInt(transform.position.y + offset) - offset;
-            //float y = Mathf.RoundToInt(transform.position.y * 2) / 2f;
+            //float y = Mathf.RoundToInt(target.transform.position.y * 2) / 2f;
             transform.position = new Vector3(transform.position.x, y, transform.position.z);
         }
         else
         {
             float x = Mathf.RoundToInt(transform.position.x + offset) - offset;
-            //float x = Mathf.RoundToInt(transform.position.x * 2) / 2f;
+            //float x = Mathf.RoundToInt(target.transform.position.x * 2) / 2f;
             transform.position = new Vector3(x, transform.position.y, transform.position.z);
         }
     }
