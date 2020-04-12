616aaaa5d33e7d254ba71785b928a3ddab57ec5e
Alejandro Gómez <algono@inf.upv.es>
2020-04-12 13:00:05 +0200

    Fix bug CompletaElCamino ImpulseZone

--- Assets/Scripts/Puzzles/CompletaElCamino/PipeImpulseZone.cs
+++ Assets/Scripts/Puzzles/CompletaElCamino/PipeImpulseZone.cs
@@ -4,23 +4,28 @@ public class PipeImpulseZone : MonoBehaviour
 {
     public Vector2 ImpulseFactor;
 
-    [Header("Disable impulse by direction")]
-    public bool DisableImpulseUp;
-    public bool DisableImpulseLeft, DisableImpulseRight, DisableImpulseDown;
+    [Header("Freeze impulse by direction")]
+    public bool FreezeImpulseUp;
+    public bool FreezeImpulseLeft, FreezeImpulseRight, FreezeImpulseDown;
 
-    private bool ImpulseEnabled => !((DisableImpulseUp && transform.right == Vector3.up)
-                     || (DisableImpulseDown && transform.right == Vector3.down)
-                     || (DisableImpulseLeft && transform.right == Vector3.left)
-                     || (DisableImpulseRight && transform.right == Vector3.right));
+    private bool ImpulseFrozen => (FreezeImpulseUp && transform.right == Vector3.up)
+                     || (FreezeImpulseDown && transform.right == Vector3.down)
+                     || (FreezeImpulseLeft && transform.right == Vector3.left)
+                     || (FreezeImpulseRight && transform.right == Vector3.right);
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Rigidbody2D rb = collision.attachedRigidbody;
         if (rb != null && Vector2.Angle(rb.velocity, (transform.up * -1)) < 45f)
         {
-            rb.AddForce(transform.right * ImpulseFactor, ForceMode2D.Impulse);
+            if (ImpulseFrozen)
+            {
+                rb.velocity = Vector2.zero;
+            }
+            else
+            {
+                rb.AddForce(transform.right * ImpulseFactor, ForceMode2D.Impulse);
+            }
         }
     }
-
-    public void CheckRotationStatus() => this.gameObject.SetActive(ImpulseEnabled);
 }
