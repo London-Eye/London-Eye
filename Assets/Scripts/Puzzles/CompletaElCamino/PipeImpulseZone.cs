f00ea514928163c32f6d950b333f4f7345055fed
Alejandro Gómez <algono@inf.upv.es>
2020-04-15 21:26:35 +0200

    Improved Impulse Zones CompletaElCamino

--- Assets/Scripts/Puzzles/CompletaElCamino/PipeImpulseZone.cs
+++ Assets/Scripts/Puzzles/CompletaElCamino/PipeImpulseZone.cs
@@ -1,31 +1,67 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 public class PipeImpulseZone : MonoBehaviour
 {
     public Vector2 ImpulseFactor;
+    public Vector2 ImpulseDirection;
+    public Space directionSpace;
+    public bool relativeToCurrentVelocity = false;
 
-    [Header("Freeze impulse by direction")]
-    public bool FreezeImpulseUp;
-    public bool FreezeImpulseLeft, FreezeImpulseRight, FreezeImpulseDown;
+    public List<Vector2> allowedEntryDirections;
 
-    private bool ImpulseFrozen => (FreezeImpulseUp && transform.right == Vector3.up)
-                     || (FreezeImpulseDown && transform.right == Vector3.down)
-                     || (FreezeImpulseLeft && transform.right == Vector3.left)
-                     || (FreezeImpulseRight && transform.right == Vector3.right);
+    [Header("Disable impulse by direction")]
+    public bool DisableImpulseUp;
+    public bool DisableImpulseLeft, DisableImpulseRight, DisableImpulseDown;
+
+    [Tooltip("If true, it will change the velocity to 0, instead of not doing anything")]
+    public bool freezeOnDisabled;
+
+    private Vector2 CalculatedImpulseDirection(Vector2 velocity)
+    {
+        Vector2 impulseDirection = (directionSpace
+            == Space.Self ? (Vector2)transform.TransformDirection(ImpulseDirection) : ImpulseDirection);
+
+        if (relativeToCurrentVelocity) impulseDirection += velocity;
+
+        return impulseDirection.normalized;
+    }
+
+    private bool IsImpulseDisabled(Vector2 velocity)
+    {
+        Vector2 impulseDirection = CalculatedImpulseDirection(velocity);
+
+        return (DisableImpulseUp && IsCloserToDirection(impulseDirection, Vector2.up))
+        || (DisableImpulseDown && IsCloserToDirection(impulseDirection, Vector2.down))
+        || (DisableImpulseLeft && IsCloserToDirection(impulseDirection, Vector2.left))
+        || (DisableImpulseRight && IsCloserToDirection(impulseDirection, Vector2.right));
+    }
+
+    private bool IsCloserToDirection(Vector2 vector, Vector2 direction)
+        => Vector2.Angle(vector, direction) < 45f;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Rigidbody2D rb = collision.attachedRigidbody;
-        if (rb != null && Vector2.Angle(rb.velocity, (transform.up * -1)) < 45f)
+        if (rb != null && IsEntryDirectionAllowed(rb.velocity))
         {
-            if (ImpulseFrozen)
+            if (IsImpulseDisabled(rb.velocity))
             {
-                rb.velocity = Vector2.zero;
+                if (freezeOnDisabled)
+                {
+                    rb.velocity = Vector2.zero;
+                }
             }
             else
             {
-                rb.AddForce(transform.right * ImpulseFactor, ForceMode2D.Impulse);
+                rb.AddForce(CalculatedImpulseDirection(rb.velocity) * ImpulseFactor, ForceMode2D.Impulse);
             }
         }
     }
+
+    private bool IsEntryDirectionAllowed(Vector2 entryDirection) => allowedEntryDirections == null
+        || allowedEntryDirections.Count == 0
+        || allowedEntryDirections.Any(allowedDirection 
+            => IsCloserToDirection(entryDirection, transform.TransformDirection(allowedDirection)));
 }
